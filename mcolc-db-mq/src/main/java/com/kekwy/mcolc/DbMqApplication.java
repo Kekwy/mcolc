955866142d35e0f010ca933e53a5bae67f864c00
Kekwy <keekkewy@qq.com>
2024-04-20 14:09:11 +0800

    feat: 新增数据库消息队列项目框架

---
+++ mcolc-db-mq/src/main/java/com/kekwy/mcolc/DbMqApplication.java
@@ -0,0 +1,13 @@
+package com.kekwy.mcolc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbMqApplication.class, args);
+    }
+
+}
