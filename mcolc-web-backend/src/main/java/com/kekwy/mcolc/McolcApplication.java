cc19a9809a62a19798a99816d279082ea3a90bc6
Kekwy <keekkewy@qq.com>
2024-04-20 09:56:11 +0800

    feat: 新增后端项目框架-1

---
+++ mcolc-web-backend/src/main/java/com/kekwy/mcolc/McolcApplication.java
@@ -0,0 +1,13 @@
+package com.kekwy.mcolc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McolcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McolcApplication.class, args);
+    }
+
+}
---
+++ mcolc-web-backend/src/test/java/com/kekwy/mcolc/McolcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kekwy.mcolc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McolcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
