3a65576376efe0405751cd29c699f8bea854f865
Kekwy <keekkewy@qq.com>
2024-04-22 00:55:41 +0800

    feat: 实现重定向接口。

--- mcolc-web-backend/src/main/java/com/kekwy/mcolc/controller/AuthController.java
+++ mcolc-web-backend/src/main/java/com/kekwy/mcolc/controller/AuthController.java
@@ -2,14 +2,17 @@ package com.kekwy.mcolc.controller;
 
 
 import com.kekwy.mcolc.controller.api.AuthAPI;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 public class AuthController implements AuthAPI {
 
     @Override
-    public String  redirectToNmoSkinAuthPage() {
-        return "redirect://www.baidu.com";
+    public void redirectToNmoSkinAuthPage(HttpServletResponse response) throws IOException {
+        response.sendRedirect("https://baidu.com");
     }
 
     @Override
--- mcolc-web-backend/src/main/java/com/kekwy/mcolc/controller/api/AuthAPI.java
+++ mcolc-web-backend/src/main/java/com/kekwy/mcolc/controller/api/AuthAPI.java
@@ -1,12 +1,15 @@
 package com.kekwy.mcolc.controller.api;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
 
 @RequestMapping("/auth")
 public interface AuthAPI {
     @GetMapping("/nmo_skin")
-    String  redirectToNmoSkinAuthPage();
+    void redirectToNmoSkinAuthPage(HttpServletResponse response) throws IOException;
 
     @GetMapping("/microsoft")
     String  redirectToMicrosoftAuthPage();
