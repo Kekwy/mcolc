ea84ceecdc12aa0df847ddc3af6e5b89608d10f8
Kekwy <keekkewy@qq.com>
2024-04-21 19:35:53 +0800

    feat: 登录界面设置随机背景图片

--- mcolc-web-frontend/src/view/Login.vue
+++ mcolc-web-frontend/src/view/Login.vue
@@ -1,13 +1,69 @@
 <template>
-  <div>hello world!</div>
+<!-- 设置随机背景图片 -->
+  <div class="common-layout" id="building" :style="{ backgroundImage: 'url(\'' + this.backgroundImageUrl + '\')' }">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+  </div>
 </template>
 
+
 <script>
+import {Message} from "element-ui";
+
 export default {
-  name: "Login"
+  name: "Login",
+  mounted() {
+    // Message.success(this.backgroundImageUrl);
+  },
+  data() {
+    return {
+      backgroundImageIndexBound: [0, 2],
+    }
+  },
+  computed: {
+    backgroundImageUrl() {
+      return '/images/background' +
+          this.random(
+              this.backgroundImageIndexBound[0],
+              this.backgroundImageIndexBound[1])
+          + '.png';
+    }
+  },
+  methods: {
+    random(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+  }
 }
 </script>
 
 <style scoped>
+login {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  border: 0
+}
+
+#building {
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  background-repeat: no-repeat;
+  /* 使背景图片居中并保持比例 */
+  background-size: cover;
+  background-position: center;
+}
 
 </style>
