8d3837bfefecf8793aed64e8dd59cbf87e84473d
Kekwy <keekkewy@qq.com>
2024-05-15 17:44:45 +0800

    feat: 后端在返回物品图标时使用CacheControl设置缓存

--- mcolc-web-backend/src/main/java/com/kekwy/mcolc/controller/ItemController.java
+++ mcolc-web-backend/src/main/java/com/kekwy/mcolc/controller/ItemController.java
@@ -4,9 +4,12 @@ import com.kekwy.mcolc.controller.api.ItemAPI;
 import com.kekwy.mcolc.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
+import org.springframework.http.CacheControl;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class ItemController implements ItemAPI {
@@ -22,7 +25,9 @@ public class ItemController implements ItemAPI {
     public ResponseEntity<Resource> icon(String key) {
         Resource resource = itemService.getIcon(key);
         if (resource != null) {
-            return ResponseEntity.ok().contentType(MediaType.IMAGE_PNG).body(resource);
+            return ResponseEntity.ok()
+                    .cacheControl(CacheControl.maxAge(30, TimeUnit.DAYS))
+                    .contentType(MediaType.IMAGE_PNG).body(resource);
         } else {
             return ResponseEntity.notFound().build();
         }
